dc68a87c974efc7c53482c68ee810cce02e3677e
jesuskata <rzarate.alex@gmail.com>
2018-11-07 18:23:01 -0600

    Añadimos la funcionalidad de identificar los titles vacíos y agregarles titles según el nombre del artículo que nos da la url.

--- web_scrapper_curso_data_eng/newspaper_receipe.py
+++ web_scrapper_curso_data_eng/newspaper_receipe.py
@@ -15,7 +15,8 @@ def main(filename):
     df = _read_data(filename)
     newspaper_uid = _extract_newspaper_uid(filename) # vamos a extraer la primera palabra del filename
     df = _add_newspaper_uid_column(df, newspaper_uid) # vamos a añadir la columna newspaper_uid al DataFrame(df)
-    df = _extract_host(df)
+    df = _extract_host(df) # vamos a añadir a la columan host los host que capturemos de la columna url
+    df = _fill_missing_titles(df) # vamos a llenar los titulos vacíos
 
     return df
 
@@ -44,6 +45,21 @@ def _add_newspaper_uid_column(df, newspaper_uid):
 def _extract_host(df):
     logger.info('Extracting host from url')
     df['host'] = df['url'].apply(lambda url: urlparse(url).netloc) # vamos a extraer solo el host de la columna url del df
+
+    return df
+
+
+def _fill_missing_titles(df):
+    logger.info('Filling the empty titles')
+    missing_titles_mask = df['title'].isna()
+
+    missing_titles = (df[missing_titles_mask]['url']
+        .str.extract(r'(?P<missing_titles>[^/]+)$')
+        .applymap(lambda title: title.split('-'))
+        .applymap(lambda title_word_list: ' '.join(title_word_list))
+    )
+
+    df.loc[missing_titles_mask, 'title'] = missing_titles.loc[:, 'missing_titles']
 
     return df
 
